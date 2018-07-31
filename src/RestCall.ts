8ad8cc7980a78df92dcbeed4e0a0528191c24b97
Geert van der Cruijsen <geert@vdcruijsen.net>
2018-07-31 20:56:46 +0200

    added a rest call that works

---
+++ src/RestCall.ts
@@ -0,0 +1,38 @@
+import { authTokenManager } from "VSS/Authentication/Services";
+export function callApi<T>(
+    url: string,
+    method: string,
+    headers: {[header: string]: string} | undefined,
+    data: any | undefined,
+    success: (response: T) => void,
+    failure: (error: TfsError, errorThrown: string, status: number) => void,
+) {
+    VSS.getAccessToken().then((sessionToken) => {
+        const authorizationHeaderValue = authTokenManager.getAuthorizationHeader(sessionToken);
+        $.ajax({
+            url,
+            method,
+            data: data || "",
+            success,
+            error: (jqXHR, _, errorThrown) => {
+                if (jqXHR.responseJSON || 401 !== jqXHR.status && 403 !== jqXHR.status) {
+                    if (jqXHR.responseJSON) {
+                        failure(jqXHR.responseJSON, errorThrown, jqXHR.status);
+                    } else {
+                        failure({name: "CallFailure", message: "call failed with status code " + jqXHR.status}, errorThrown, jqXHR.status);
+                    }
+                } else {
+                    failure({name: "AuthorizationFailure", message: "unauthorized call"}, errorThrown, jqXHR.status);
+                }
+            },
+            beforeSend: (jqXHR) => {
+                jqXHR.setRequestHeader("Authorization", authorizationHeaderValue);
+                if (headers) {
+                    for (const header in headers) {
+                        jqXHR.setRequestHeader(header, headers[header]);
+                    }
+                }
+            },
+        } as JQueryAjaxSettings);
+    });
+}
--- src/compliancywidget.ts
+++ src/compliancywidget.ts
@@ -3,67 +3,70 @@ import Core_Client = require("TFS/Core/RestClient");
 import System_Contracts = require("VSS/Common/Contracts/System");
 import Service = require("VSS/Service");
 import WebApi_Constants = require("VSS/WebApi/Constants");
-import VstsRestApi = require("VSS/WebApi/RestClient");
+import VstsRestApi = require("VSS/WebApi/RestClient"); 
 
 import * as BuildServices from "TFS/Build/RestClient";
 import * as ReleaseServices from "ReleaseManagement/Core/RestClient";
 import * as GitServices from "TFS/VersionControl/GitRestClient";
 
+import { callApi} from "./RestCall";
+
+
 import { Artifact } from "ReleaseManagement/Core/Contracts";
 
 export class CompliancyWidget {
 	private renderValues: any = {};
-
+	
 	constructor(
 		public WidgetHelpers : any) { }
-
-	public load() {
-		return this.showCompliancy();
-	}
-
-	public reload() {
-		return this.showCompliancy();
-	}
-
-	private async showCompliancy() {
-		try {
-			
-			var webcontext = VSS.getWebContext();
-			var project = {
-				name : webcontext.project.name,
-				teamId: webcontext.team.id,
-				id: webcontext.project.id,
-				uri: webcontext.host.uri
-			};
-
-			//this.renderWidget();
-
-			var releaseClient = ReleaseServices.getClient();
-			var deployment = await releaseClient.getDeployments("CompliancyWidget",undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,1,undefined,undefined);
-			
-			const $title = $("#title");
-			if(deployment != null && deployment.length > 0)
-			{
-				var artifact = deployment[0].release.artifacts.find(a => a.type === "Git");
-				$title.text((artifact as Artifact).definitionReference["branches"].name);
+		
+		public load() {
+			return this.showCompliancy();
+		}
+		
+		public reload() {
+			return this.showCompliancy();
+		}
+		
+		private async showCompliancy() {
+			try {
+				
+				var webcontext = VSS.getWebContext();
+				var project = {
+					name : webcontext.project.name,
+					teamId: webcontext.team.id,
+					id: webcontext.project.id,
+					uri: webcontext.host.uri
+				};
+				
+				//this.renderWidget();
+				
+				var releaseClient = ReleaseServices.getClient();
+				var deployment = await releaseClient.getDeployments("CompliancyWidget",undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,1,undefined,undefined);
+				
+				const $title = $("#title");
+				if(deployment != null && deployment.length > 0)
+				{
+					var artifact = deployment[0].release.artifacts.find(a => a.type === "Git");
+					$title.text((artifact as Artifact).definitionReference["branches"].name);
+				}
+				
+				await callApi(project.uri+ "/_apis/projects","GET",undefined,undefined,(result: any) => {
+					alert("success " +result.count);
+				},(error:any)=> {
+					alert("error"+error);
+				});
+				
+				
+				return this.WidgetHelpers.WidgetStatusHelper.Success();
+			} catch (e) {
 			}
-
-			// var client = Service.VssConnection
-			// 	.getConnection()
-			// 	.getHttpClient(Core_Client.CoreHttpClient, WebApi_Constants.ServiceInstanceTypes.TFS);
-
-			// var restclient = new VstsRestApi.VssHttpClient(project.uri);
-
+		}
+		
+		
+		private renderWidget() {
+			const $title = $("#title");
 			
-			return this.WidgetHelpers.WidgetStatusHelper.Success();
-		} catch (e) {
+			$title.text("test van xpirit!!!!");
 		}
 	}
-
-
-	private renderWidget() {
-		const $title = $("#title");
-
-		$title.text("test van xpirit!!!!");
-	}
-}
